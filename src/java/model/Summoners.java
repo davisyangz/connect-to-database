d1670bb9ff8d50e433368a6687668705506a20c1
davisyangz <yangzhengdavis@gmail.com>
2015-10-15 00:02:27 -0500

    added model,dbcon.properties, and ojdbc6.jar

---
+++ src/java/model/Summoners.java
@@ -0,0 +1,66 @@
+
+package model;
+
+public class Summoners {
+    
+    private int MonsterID;
+    private String MonsterName;
+    private String property;
+    private int MLevel;
+
+    public Summoners() {
+        this.MonsterID = 0;
+        this.MonsterName = "";
+        this.property = "";
+        this.MLevel = 0;
+    }
+    
+    
+    public Summoners(int MonsterID, String MonsterName, String property, int MLevel) {
+        this.MonsterID = MonsterID;
+        this.MonsterName = MonsterName;
+        this.property = property;
+        this.MLevel = MLevel;
+    }
+
+    public int getMonsterID() {
+        return MonsterID;
+    }
+
+    public void setMonsterID(int MonsterID) {
+        this.MonsterID = MonsterID;
+    }
+
+    public String getMonsterName() {
+        return MonsterName;
+    }
+
+    public void setMonsterName(String MonsterName) {
+        this.MonsterName = MonsterName;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public int getMLevel() {
+        return MLevel;
+    }
+
+    public void setMLevel(int MLevel) {
+        this.MLevel = MLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "Summoners{" + "MonsterID=" + MonsterID + ", MonsterName=" + MonsterName + ", property=" + property + ", MLevel=" + MLevel + '}';
+    }
+    
+    
+    
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : index
+    Created on : Oct 14, 2015, 10:27:39 PM
+    Author     : Zheng
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Monsters</title>
+    </head>
+    <body>
+        <h1>My monster in summoners war</h1>
+        
+        <a href="read">View all my monsters</a>
+    </body>
+</html>
